1fb2b3d33421b2dea667a4a29e91a8bbd9647d26
Liu Xu <liuxu@365ime.com>
2016-02-19 11:41:11 +0800

    创建项目

---
+++ OneSunChat/OneSunChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OneSunChat
+//
+//  Created by 刘ToTo on 16/2/19.
+//  Copyright © 2016年 刘ToTo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
